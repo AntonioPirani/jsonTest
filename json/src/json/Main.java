ce0ad740c523d379cabd2f574c76c27a3e3e1fd2
Antonio Pirani <s1098080@studenti.univpm.it>
2021-12-05 18:50:40 +0100

    ulteriore modifica file e conversione finale funzionante

--- json/src/json/Main.java
+++ json/src/json/Main.java
@@ -5,13 +5,15 @@ package json;
 
 import java.io.*;
 import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Prova run #2");
+		System.out.println("Prova run #3");
 		String next;
 		
 		String s="";
@@ -30,15 +32,42 @@ public class Main {
 				
 			} while (next != null);
 			
-			System.out.println(s);
+			//System.out.println(s);
 			
 			//Object obj = JSONValue.parse(s);
 		   // JSONArray array = (JSONArray)obj;
 			//System.out.println("Stampa di obj: \n" + obj);
 			
-			JSONObject obj = (JSONObject)JSONValue.parse(s);
-			String id = (String) obj.get("id");
-			System.out.println("Impiegato: "+id);
+//			JSONObject obj = (JSONObject)JSONValue.parse(s);
+//			String id = (String) obj.get("id");
+//			System.out.println("ID: "+id);
+			
+			// convert json to Java
+		    //JSONObject obj = (JSONObject) JSONValue.parse(json);
+			
+			JSONParser parser = new JSONParser();
+			try {
+				
+				JSONObject json = (JSONObject)parser.parse(s);
+				JSONArray results = (JSONArray) json.get("results");
+				for (int i = 0; i < results.size(); i++) {
+				    // obtaining the i-th result
+					JSONObject result = (JSONObject) results.get(i);
+				    JSONObject geometry = (JSONObject) result.get("geometry");
+				    JSONObject location = (JSONObject) geometry.get("location");
+				    System.out.println(location.get("lat"));
+				    System.out.println(location.get("lng"));
+				}
+				
+				String status = (String )json.get("status");
+				System.out.println("Status: "+status);
+				
+				
+				
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 		    
 		}
